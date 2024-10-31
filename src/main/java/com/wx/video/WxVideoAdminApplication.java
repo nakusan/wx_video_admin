905a3973bf628107082df5d33f7a96a08e74fa4a
nakusan <yunpeng.luo@outlook.com>
2024-10-31 17:01:27 +0800

    update
    update

--- src/main/java/com/wx/video/WxVideoAdminApplication.java
+++ src/main/java/com/wx/video/WxVideoAdminApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@MapperScan("com.wx.video.mapper")
 @EnableCaching  // 开启缓存
 public class WxVideoAdminApplication {
 
--- src/main/java/com/wx/video/config/MyBatisPlusConfig.java
+++ src/main/java/com/wx/video/config/MyBatisPlusConfig.java
@@ -2,10 +2,19 @@ package com.wx.video.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
+@MapperScan("com.wx.video.mapper")
+@EnableTransactionManagement //启用事务管理
 public class MyBatisPlusConfig {
 
     /**
